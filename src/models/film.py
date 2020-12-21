b8bcbc0746d074eb090ff71203ed1b4a484abc75
Aleksandr Kozlov <avlkozlov@avito.ru>
2020-12-22 02:04:44 +0300

    Исправил проблему с сериализацией данных через orjson

--- src/models/film.py
+++ src/models/film.py
@@ -4,6 +4,11 @@ import orjson
 from pydantic import BaseModel
 
 
+def orjson_dumps(v, *, default):
+    # orjson.dumps returns bytes, to match standard json.dumps we need to decode
+    return orjson.dumps(v, default=default).decode()
+
+
 class Film(BaseModel):
     id: str
     title: str
@@ -12,4 +17,4 @@ class Film(BaseModel):
     class Config:
         # Заменяем стандартную работу с json на более быструю
         json_loads = orjson.loads
-        json_dumps = orjson.dumps
+        json_dumps = orjson_dumps
